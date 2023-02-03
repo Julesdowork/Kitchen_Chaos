7218645136478ff9550cf0a0a74181ead438517b
Julian McNeill <julesdowork@gmail.com>
2023-02-03 17:01:49 -0500

    Animations

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Player _player;
+
+    private Animator _animator;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _animator.SetBool(IS_WALKING, _player.IsWalking());
+    }
+}
