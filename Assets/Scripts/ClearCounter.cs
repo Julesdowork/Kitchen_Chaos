e97e48faa07e87d29252204defac0dc22fbacf51
Julian McNeill <julesdowork@gmail.com>
2023-02-05 00:22:31 -0500

    Kitchen Object, Scriptable Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+    [SerializeField] private Transform _topPoint;
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform kitchenObject = Instantiate(_kitchenObjectSO.Prefab, _topPoint);
+        kitchenObject.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObject.GetComponent<KitchenObject>().KitchenObjectSO.ObjectName);
     }
 }
