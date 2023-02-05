52784a7b36d38f7ba46cc7cb08cffe2cc2174c90
Julian McNeill <julesdowork@gmail.com>
2023-02-05 00:47:03 -0500

    Kitchen Object Parent

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,13 +6,53 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
     [SerializeField] private Transform _topPoint;
+    [SerializeField] private ClearCounter _secondClearCounter;
+    [SerializeField] private bool _testing;
+
+    private KitchenObject _kitchenObject;
+
+    public KitchenObject KitchenObject
+    {
+        get => _kitchenObject;
+        set => _kitchenObject = value;
+    }
+
+    void Update()
+    {
+        if (_testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (_kitchenObject != null)
+            {
+                _kitchenObject.ClearCounter = _secondClearCounter;
+            }
+        }
+    }
 
     public void Interact()
     {
-        Debug.Log("Interact");
-        Transform kitchenObject = Instantiate(_kitchenObjectSO.Prefab, _topPoint);
-        kitchenObject.localPosition = Vector3.zero;
+        if (_kitchenObject == null)
+        {
+            Transform kitchenObject = Instantiate(_kitchenObjectSO.Prefab, _topPoint);
+            kitchenObject.GetComponent<KitchenObject>().ClearCounter = this;
+        }
+        else
+        {
+            Debug.Log(_kitchenObject.ClearCounter);
+        }
+    }
 
-        Debug.Log(kitchenObject.GetComponent<KitchenObject>().KitchenObjectSO.ObjectName);
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return _topPoint;
+    }
+
+    public void ClearKitchenObject()
+    {
+        _kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return _kitchenObject != null;
     }
 }
