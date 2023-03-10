c5c9dff723fb15b63280ddca6ec4a50dd2b56bf8
Julian McNeill <julesdowork@gmail.com>
2023-03-10 15:44:34 -0500

    Sync Player Animations

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
@@ -17,6 +18,11 @@ public class PlayerAnimator : MonoBehaviour
 
     void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
+
         _animator.SetBool(IS_WALKING, _playerMovement.IsWalking());
     }
 }
